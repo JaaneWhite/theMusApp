b0d7284ba434410a3a5a7ac3cee8e2d7d24bde56
Anastasia <a.shironosova@altsoft.spb.ru>
2020-11-18 17:36:38 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300&display=swap');
+@media (min-width: 1500px) {
+  .container {
+    max-width: 1460px;
+  }
+}
+body {
+  margin: auto;
+  font-family: 'Roboto', sans-serif;
+  color: #270000;
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-row class="header-row">
+    <b-container>
+      Привет
+    </b-container>
+  </b-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.header-row {
+  height: 425px;
+  background-color: red;
+}
+/* планшетная версия - 768-1024 px*/
+@media (min-width: 768px) {
+
+}
+/* пк версия более 1024 px*/
+@media (min-width: 1024px) {
+
+}
+</style>
