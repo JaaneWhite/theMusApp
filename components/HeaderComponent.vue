f6373f0f71d0a0ede376c0291e3daabc61fed8dc
Anastasia Shironosova <a.shironosova@altsoft.spb.ru>
2020-11-19 12:39:22 +0300

    + карусель в хедере на главной

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,31 +1,54 @@
 <template>
   <header>
     <b-row class="header-row">
-      <b-container>
-        <!--верхнее меню - бургер -->
-        <b-row class="burger-menu-row">
+      <!--слайдер с картинками -->
+      <b-row class="header-carousel-row">
+        <b-col cols="6" class="header-left">
+        </b-col>
+        <b-col cols="6" xl="6" class="header-right">
+          <b-carousel class="header-carousel"
+                      id="carousel-1"
+                      v-model="slide"
+                      :interval="4000"
+          >
+            <b-carousel-slide class="slide-img"
+                              img-src="~assets/img/slider-1.png">
+            </b-carousel-slide>
+            <b-carousel-slide class="slide-img"
+                              img-src="~assets/img/slider-2.png">
+            </b-carousel-slide>
+            <b-carousel-slide class="slide-img"
+                              img-src="~assets/img/slider-3.png">
+            </b-carousel-slide>
+
+          </b-carousel>
+        </b-col>
+      </b-row>
+      <b-row class="header-content">
+        <b-container >
+          <!--верхнее меню - бургер -->
+          <b-row class="burger-menu-row">
           <ToggleMenu />
         </b-row>
-        <b-row class="project-text">
-          Портал создан на средства гранта Президента Российской Федерации для поддержки
-          творческих проектов общенационального значения в области культуры и искусства
-        </b-row>
-        <!-- основной блок с заголовком и поиском -->
-        <b-row>
-          <b-col cols="12" xl="7" class="title-col">
-
-          <b-row class="title-text">ТЕАТРАЛЬНЫЕ МУЗЕИ И АРХИВЫ РОССИИ <br>И РУССКОГО ЗАРУБЕЖЬЯ</b-row>
-        </b-col>
-          <b-col cols="12" xl="1" class="empty-block"></b-col>
-          <b-col cols="12" xl="4" class="search-block">
-          <b-row class="search-row">
-            <b-form class="search-form">
-              <b-form-input id="search-input" placeholder="Поиск" type="search"></b-form-input>
-              <b-button class="search-button keyboard-button"></b-button>
-              <b-img src="~assets/img/divider.png" class="divider-img"></b-img>
-              <b-button class="search-button sb-button"></b-button>
-            </b-form>
+          <b-row class="project-text-top">
+            {{projectText}}
           </b-row>
+          <!-- основной блок с заголовком и поиском -->
+          <b-row class="header-content-row">
+            <b-col cols="12" xl="7" class="title-col">
+              <b-row class="project-text-center">{{projectText}}</b-row>
+              <b-row class="title-text">ТЕАТРАЛЬНЫЕ МУЗЕИ И АРХИВЫ РОССИИ <br>И РУССКОГО ЗАРУБЕЖЬЯ</b-row>
+            </b-col>
+            <b-col cols="12" xl="1" class="empty-block"></b-col>
+            <b-col cols="12" xl="4" class="search-block">
+              <b-row class="search-row">
+                <b-form class="search-form">
+                  <b-form-input id="search-input" placeholder="Поиск" type="search"></b-form-input>
+                  <b-button class="search-button keyboard-button"></b-button>
+                  <b-img src="~assets/img/divider.png" class="divider-img"></b-img>
+                  <b-button class="search-button sb-button"></b-button>
+                </b-form>
+              </b-row>
           <b-row class="link-row">
             <b-col cols="9" class="left-link"><b-link to="#" class="search-link">Расширенный поиск</b-link></b-col>
             <b-col cols="3" class="right-link"><b-link to="#" class="search-link">А - Я</b-link></b-col>
@@ -33,6 +56,7 @@
         </b-col>
         </b-row>
       </b-container>
+      </b-row>
     </b-row>
     <!-- развернутое меню для планшетной и пк-версий -->
     <b-row class="menu-row">
@@ -49,7 +73,12 @@ import TopMenu from "~/components/TopMenu";
 import ToggleMenu from "~/components/ToggleMenu";
 export default {
   name: "HeaderComponent",
-  components: {ToggleMenu, TopMenu}
+  components: {ToggleMenu, TopMenu},
+  data() {
+    return {
+      projectText: "Проект осуществлён на средства гранта Президента Российской Федерации. Программа для поддержки творческих проектов общенационального значения в области культуры и искусства области культуры.",
+    }
+    }
 }
 </script>
 
@@ -60,11 +89,14 @@ export default {
     height: 429px;
     background-image: url("~assets/img/header bg.png");
   }
+  .header-carousel-row {
+    display: none;
+  }
 
   .burger-menu-row {
     height: 80px;
   }
-  .project-text {
+  .project-text-center {
     display: none;
   }
   .title-col {
@@ -175,6 +207,9 @@ export default {
 
 /* планшетная версия - 768-1024 px*/
 @media (min-width: 768px) {
+  .header-row {
+    height: 474px;
+  }
   .burger-menu-row {
     display: none;
   }
@@ -192,7 +227,7 @@ export default {
     line-height: 1.333;
     margin: 60px auto 45px auto;
   }
-  .project-text {
+  .project-text-top {
     display: flex;
     text-align: center;
     font-size: 12px;
@@ -212,4 +247,73 @@ export default {
   }
 
 }
+
+/* пк версия - 1024 - ... px*/
+@media (min-width: 1024px) {
+  .header-row {
+    height: 453px;
+
+  }
+  .header-carousel-row {
+    display: contents;
+    height: 453px;
+  }
+  .header-content {
+    position: absolute;
+    z-index: 10;
+    top: 0;
+    width: 100%;
+  }
+  .header-content-row {
+    flex-direction: row;
+    padding-top: 45px;
+  }
+  .search-block {
+    padding: 0;
+    margin-top: 0;
+  }
+  .header-left {
+    padding: 0;
+
+  }
+  .header-right {
+    display: flex;
+    text-align: right;
+    padding: 0;
+  }
+  .header-carousel {
+    height: 453px;
+    clip-path: polygon(25% 0%, 100% 0%, 100% 100%, 0% 100%);
+  }
+  .project-text-top {
+    display: none;
+  }
+  .project-text-center {
+    display: flex;
+    color: white;
+    font-size: 14px;
+    text-align: left;
+    padding-left: 0;
+
+  }
+  .title-col {
+    padding-left: 0;
+    flex-direction: column;
+  }
+  .title-text {
+    font-size: 36px;
+    line-height: 1.389;
+    text-align: left;
+    padding: 0;
+    margin-left: 0;
+    margin-top: 100px;
+  }
+  .menu-row {
+    z-index: 20;
+
+  }
+}
+
+
+
 </style>
