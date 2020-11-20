f7c747dd8f5f9aa35267816b77aaf2a368148e3c
Anastasia <a.shironosova@altsoft.spb.ru>
2020-11-20 18:45:22 +0300

    + модальное окно выбора персоны

--- pages/persons.vue
+++ pages/persons.vue
@@ -19,6 +19,31 @@
 
         <AlphabetSort :alphabet-options="alphabetOptions" :alphabet-selected="alphabetSelected"/>
 
+        <b-button v-b-modal.modalPopover class="person-select-button">{{selectedPerson}}</b-button>
+
+        <b-modal id="modalPopover"  hide-footer>
+          <template #modal-header="{ close }">
+            <!-- Emulate built in modal header close button action -->
+              <div class="modal-header-title">
+                {{'Персоны - ' + alphabetSelected}}
+              </div>
+              <b-col class="modal-header-btn-block">
+                <b-button @click="close()" class="modal-close-button">
+                </b-button>
+              </b-col>
+          </template>
+          <b-list-group class="person-list-group">
+            <b-list-group-item
+              v-for="listItem in personlist"
+              @click="selectedPerson = listItem.text; $bvModal.hide('modalPopover')"
+              class="person-list-item"
+            >
+              {{listItem.text}}
+            </b-list-group-item>
+          </b-list-group>
+
+        </b-modal>
+
       </b-col>
       <b-col cols="12" md="9" class="content-col">
         <b-row class="person-title">Брехт Бертольт</b-row>
@@ -59,6 +84,7 @@ export default {
       pageTitleText: 'Персоны',
       personListPaginationAreaControls: 'person-name-list',
       personNameListPerPage: '20',
+      selectedPerson: 'Персоны',
       breadCrumbItems: [
         {
           text: 'Главная',
@@ -188,7 +214,63 @@ export default {
   .person-col {
     padding: 0;
   }
+  .person-select-button, .person-select-button:hover, .person-select-button:focus, .person-select-button:active {
+    border: solid #cf6a40 3px;
+    width: 100%;
+    border-radius: 0;
+    background-color: white;
+    color: #270000;
+    box-shadow: none;
+    height: 40px;
+    overflow: hidden;
+    padding-right: 15%;
+    text-align: left;
+    font-weight: bold;
+    font-size: 14px;
+    background-image: url("~assets/img/list-arrows.png");
+    background-repeat: no-repeat;
+    background-position-y: center;
+    background-position-x: 95%;
+  }
+  .modal-content {
+    border-radius: 0;
+    border: solid #cf6a40 3px;
+  }
+  .modal-header-title {
+    width: 80%;
+    display: flex;
+    font-size: 26px;
+    font-family: 'Roboto', sans-serif;
+    align-items: center;
+    color: #270000;
+    padding-top: 5px;
+  }
+  .modal-header-btn-block {
+    width: 20%;
+  }
 
+.modal-close-button, .modal-close-button:hover, .modal-close-button:active, .modal-close-button:focus {
+  background-image: url("~assets/img/menu close.png");
+  background-repeat: no-repeat;
+  background-position-x: right;
+  background-position-y: center;
+  background-color: white;
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 0;
+  margin-right: 0;
+}
+.person-list-group {
+  border: none;
+  border-radius: 0;
+  overflow: scroll;
+  height: 600px;
+}
+.person-list-item {
+  border: none;
+  border-bottom: solid #9f959d 1px;
+}
 }
 /* планшетная версия - 768-1024 px*/
 @media (min-width: 768px) {
